b5631d1da2eedcf6df9c0cef1de422fd72b79e00
dikjain <168990083+dikjain@users.noreply.github.com>
2024-08-19 17:06:08 +0530

    Update Toppg.jsx

--- src/Toppg.jsx
+++ src/Toppg.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Toppg() {
   return (
     <div className='Toppg relative w-full h-[35vh] nnn '>
-      <img src='src\favicon.jpg' className='absolute logo h-[150px] left-[20vw] top-[40px] ii'></img>
+      <img src='https://i.postimg.cc/QN7bNwvg/favicon.png' className='absolute logo h-[150px] left-[20vw] top-[40px] ii'></img>
 
         <div id="hding" className=' w-[60%] flex-col h-[80%]  nnn'>
             <div className='text-9xl mainhid font-[500] txt font-[fantasy]'>Crypto-Verse</div>
