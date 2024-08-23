198fc4353b04fb281df386a3254d99f67b3fb588
dikjain <168990083+dikjain@users.noreply.github.com>
2024-08-24 00:58:46 +0530

    Add files via upload

--- Bottompg.jsx
+++ Bottompg.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Msvg from './Msvg.jsx'
 
+
 export default function Bottompg({coins,chng,favshow,favagain,currs}) {
   let ppp = 10
   const [pg,crntpg] = useState(1)
@@ -8,7 +9,12 @@ export default function Bottompg({coins,chng,favshow,favagain,currs}) {
   const [lst,setlst] = useState(pg*ppp)  
   const [fst,setfst] = useState(pg*ppp -ppp)  
   let trimcoin = coins.slice(fst,lst)
-  let remin = 0
+let qqlli = 0
+  //gsaphook
+
+
+
+
   
   let pgsnum = []
   for(let i=1;i<=coins.length/ppp;i++){
@@ -50,7 +56,7 @@ export default function Bottompg({coins,chng,favshow,favagain,currs}) {
     <div className=' w-full h-[100vh] relative  px-[200px] Bottompg'>
       <button onClick={favshow} className='favbtn absolute h-[40px] w-[160px] rounded-xl px-5 py-2 top-[-90px] right-[20%] bg-gray-600'>Favourites</button>
       
-      <div id="pgicont" className='absolute left-[50%] text-center  w-[400px] min-h-[40px] translate-x-[-50%] bottom-[25px] nnn'>
+      <div id="pgicont" className='absolute left-[50%] text-center z-[10] w-[400px] min-h-[40px] translate-x-[-50%] bottom-[25px] nnn'>
         {pgsnum.map((ll)=>{
           return <div key={ll} id={`pgbox${ll}`} onClick={handlepg} className={`${pg===ll ? "bg-green-600 ":" bg-gray-600 "} pgbox mx-[2px] rounded-full overflow-hiddentext-center nnn `}>{ll}</div>
         })}
@@ -61,8 +67,9 @@ export default function Bottompg({coins,chng,favshow,favagain,currs}) {
           <h1 className='relative markcap left-[5vw]'>Market-Cap</h1>
           <h1 className='relative right-[-20px]'>24hr Change</h1>
         </div>
+       
       {trimcoin.map((coin,i) => (
-        <div key={coin.id} className={`${Number(coin.price_change_percentage_24h)>=0? "go":"lo"}   w-full overflow-hidden h-[7vh] px-10 py-2 my-[10px] flex justify-between items-center`}>
+        <div key={coin.id} className={`${Number(coin.price_change_percentage_24h)>=0? "go":"lo"}   coinns  w-full overflow-hidden h-[7vh] px-10 py-2 my-[10px] flex justify-between items-center`}>
           <div id="details" className='flex relative overflow-visible items-center justify-between h-full w-[20%]'>
             <div onClick={()=>handlefav(coin)} className='absolute left-[-45px]'>< Msvg col={retcol.includes(coin.market_cap_rank)?"yellow":"white"}/></div>
             <img src={coin.image} className='imgx absolute h-[110%]'></img>
--- Favpg.jsx
+++ Favpg.jsx
@@ -26,7 +26,7 @@ export default function Favpg({favdata,favshow,coins,getfavs,currs}){
 
   return (
     <div className='w-full h-[100vh] relative   gap-10 px-[200px] favgp'>
-        <div id="pgicont" className='absolute left-[50%] text-center  w-[400px] min-h-[40px] translate-x-[-50%] bottom-[25px] nnn'>
+        <div id="pgicont" className='absolute left-[50%] text-center  z-[10] w-[400px] min-h-[40px] translate-x-[-50%] bottom-[25px] nnn'>
         {pgsnum.map((ll)=>{
           return <div key={ll} id={`pgbox${ll}`} onClick={handlepg} className={`${pg===ll ? "bg-green-600 ":" bg-gray-600 "} pgbox mx-[2px] rounded-full overflow-hiddentext-center nnn `}>{ll}</div>
         })}
